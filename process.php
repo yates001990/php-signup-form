0aa9194a20d7b090a8e75efcae9af66825c60089
yates001990 <yates0@yahoo.com>
2026-04-17 18:05:04 -0400

    process.php

---
+++ process.php
@@ -0,0 +1,12 @@
+<?php
+
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+echo "<h2>Signup Successful</h2>";
+echo "Name: " . $fullname . "<br>";
+echo "Email: " . $email . "<br>";
+
+?>
+
